4948ed227c8c6bdf3a1480c25ed9dd851ecd4884
김진렬 <0205kjl@naver.com>
2026-06-19 15:19:51 +0900

    PPCS-90: - 출고 등록 및 관리 화면 구성

--- src/main/java/com/pcs/web/controller/PageController.java
+++ src/main/java/com/pcs/web/controller/PageController.java
@@ -81,6 +81,11 @@ public class PageController {
         return "forward:/outbound.html";
     }
 
+    @GetMapping("/w/{companyCode}/outbound/new")
+    public String outboundRegister() {
+        return "forward:/outbound-register.html";
+    }
+
     @GetMapping("/w/{companyCode}/inspection")
     public String inspection() {
         return "forward:/inspection.html";
